f095d3e0c5ec6fc7d53b97aaeb1821ebd7793e4b
Renato Silva <orenatodos@gmail.com>
2022-12-03 15:08:34 -0300

    feat: add new tokens on default theme

--- src/styles/themes/defaultTheme.ts
+++ src/styles/themes/defaultTheme.ts
@@ -1,17 +1,55 @@
 export const defaultTheme = {
   borderWidths: {},
-  colors: {},
+  colors: {
+    primary: '#F231A5',
+    secondary: '#3CD3C1',
+    mainBg: '#06092B',
+    white: '#FAFAFA',
+    black: '#030517',
+    lightGray: '#EAEAEA',
+    gray: '#8F8F8F',
+    darkGray: '#2E2F42'
+  },
   fonts: {
     sans: "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
   },
-  fontSizes: {},
-  fontWeights: {},
+  fontSizes: {
+    xsmall: '1.2rem',
+    small: '1.4rem',
+    medium: '1.6rem',
+    large: '1.8rem',
+    xlarge: '2.0rem',
+    xxlarge: '2.8rem'
+  },
+  fontWeights: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700
+  },
   letterSpacings: {},
   lineHeights: {},
-  radii: {},
+  radii: {
+    1: '0.4rem'
+  },
   shadows: {},
   sizes: {},
-  space: {},
+  space: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
   transitions: {},
-  zIndices: {}
+  zIndices: {
+    1: 10,
+    2: 20,
+    3: 30,
+    4: 40,
+    5: 50,
+    max: 999
+  }
 } as const
